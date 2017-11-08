659869f5e743666dd80a900a27a8bf1be57d3940
Brendon Chen <brc019@ucsd.edu>
2017-11-08 09:45:04 -0800

    Added functionality to modifying the underlying data

--- js/family2.js
+++ js/family2.js
@@ -7,7 +7,8 @@ var family = [
 ];
 
 $(document).ready(function() {
-	  // compile the template
+
+  // compile the template
   var source   = $("#list-template").html();
   var template = Handlebars.compile(source);
 
@@ -71,8 +72,8 @@ $(document).ready(function() {
 	    	var id = this.getAttribute("id");
 	    	var index = id.substr(5);
 	    	console.log(index);
-	    	var content = $("#content" + index);
-	    	var subIndex = $("#content" + index + " p").length;
+	    	// var content = $("#content" + index);
+	    	// var subIndex = $("#content" + index + " p").length;
 
 	    	console.log(document.getElementById("input" + index));
 	    	var text = document.getElementById("input" + index).value;
@@ -84,26 +85,54 @@ $(document).ready(function() {
 	    	}
 
 	    	else {
-		    	content.append("<p id='p" + index + subIndex + "''><button id='rm" + index + subIndex + "''>&times</button> " + text + "</p>");
+                family[index]["entry"].push(text);
+                displayContent(index);
 
                 // Clears the input field.
 		    	$("#input" + index).val("");
-
-                // Attaches a click handler to the remove button.
-                $("#rm" + index + subIndex).click(function() {
-                    console.log(this);
-                    $(this).parent().remove();
-                });
 		    }
     	}
     });
 
     var content = $("#content" + i + " p");
     for (var j = 0; j < content.length; j++) {
-	    $("#rm" + i + j).click(function() {
-	    	console.log(this);
-	    	$(this).parent().remove();
-	    });
+	    $("#rm" + i + j).click(removeHandler);
     }
   }
 });
+
+function displayContent(index) {
+    var content = $("#content" + index);
+    // Clears the div.
+    content.html("");
+
+    // Repopulates.
+    var array = family[index]["entry"];
+    for (var i = 0; i < array.length; i++) {
+        content.append("<p id='p" + index + i + "'' class='card'><button id='rm" + index + i + "'' class='remove-btn'>&times</button> " + array[i] + "</p>");
+
+        // Attaches a click handler to the remove button.
+        $("#rm" + index + i).click(removeHandler);
+    }
+}
+
+function removeHandler() {
+
+    console.log(this);
+
+    // Captures the id of the content div.
+    var id = $(this).parent().parent().attr("id");
+    console.log(id);
+
+    // Calculates the indicies.
+    var index = id.substr("content".length);
+    var subIndex = $(this).attr("id").substr(("rm" + index).length);
+    console.log(index);
+    console.log(subIndex);
+
+    // Removes the entry from the entry array.
+    family[index]["entry"].splice(subIndex, 1);
+
+    // Clears the div and repopulates.
+    displayContent(index);
+}
