da63158fedb9706b4f655881c41432b9e51f0554
WaynePhung <waynewphung@aol.com>
2017-11-11 23:38:09 -0800

    Updated HTML, CSS, and JS of family files.

--- js/loopList.js
+++ js/loopList.js
@@ -1,39 +1,67 @@
+//This code was adapted and inspired by Brendon Chen. Without him, much of the interactions of this page, and my knowledge of how the template would have functioned, would have been very much lost.
+
+var familyList = [
+	{"listName": "My Personal List", "itemList": ["Milk", "Water", "Juice"], "index": 0},
+	{"listName": "My Wife", "itemList": ["Milk", "Water", "Juice"], "index": 1}
+];
+
 $(document).ready(function(){
 	"use strict";
 	
-	$("form, #cancelButton1").hide();
+	var source     = $("#listTemplate").html();
+  	var template   = Handlebars.compile(source);
+	var parentDiv     = $(".famList");
 	
-	$("#addItem1").click(function(){
-		$("form, #cancelButton1").slideDown();
+	for (var i = 0; i < familyList.length; i++){
+		var dbFamList = familyList[i];
+		var db2Temp = template(dbFamList);
+		parentDiv.append(db2Temp);
 		
-	});
-	
-	$("#cancelButton1").click(function(){
-		$("form").slideUp();
-		$("#cancelButton1").hide();
-	});
-	
-	$("#textInput1").keyup(function (enter) {
-		if (enter.keyCode === 13) {
-			var id1 = this.getAttribute("id");
-			var index1 = id1.substr(9);
-			var index2 = document.getElementById("actualList" + index1).getElementsByTagName("p").length;
-			var inputVal = document.getElementById("textInput" + index1).value;
-			if ( inputVal === "" ) {
-				console.log("Nothing entered in input field.");
-				alert('Please enter an item description or click "Cancel Add".');
-			} else {
-				console.log("Item entered!");
-				/*var addNewItem = '<p><button class = "removeItem" id = "removeButton11">&times</button>' + inputVal + '</p>';
-				document.getElementById("panel-body" + index + p.length.value).appendChild(addListItem);
-				inputVal = "";
-				var getActualList = document.getElementById("actualList" + index);
-				var description = document.getElementById("textInput" + index);
-				var newItem = document.createElement("p");
-				newItem.setAttribute('id', description.value);*/
-				var addNewItem = '<p class = "item" id = "i' + index1 + index2 + '"><button class = "removeItem" id = "removeButton' + index1 + index2 + '">&times</button>' + inputVal + '</p>';
-				$("#actualList" + index1).appendChild(addNewItem);
-			} <!--dd-->
+		var panelDefault  = $("#list" + i);
+		var listName      = $("#title" + i);
+		var actualList    = $("#actualList" + i);
+		var listFooter    = $("#listFooter" + i);
+		var addItemButton = $("#addItem" + i);
+		var cancelButton  = $("#cancelButton" + i);
+		var form          = $("#descriptForm" + i);
+		var textField     = $("#textInput" + i);
+		var transition    = 250;
+		
+		if (i === 0) {
+			panelDefault.css("margin-top", "8em");
 		}
-	});
+		
+		actualList.hide();
+		listFooter.hide();
+		addItemButton.hide();
+		cancelButton.hide();
+		form.hide();
+		
+		listName.click(function(){
+			var idIndex = this.getAttribute("id").substr(5);
+			$("#actualList" + idIndex).slideToggle(transition);
+			$("#listFooter" + idIndex).slideToggle(transition);
+			$("#addItem" + idIndex).slideToggle(transition);
+			if (listName.is(":visible")){
+				$("#cancelButton" + idIndex).hide(transition);
+				$("#descriptForm" + idIndex).hide(transition);
+				$("#textInput" + idIndex).value("");
+			}
+		});
+		
+		addItemButton.click(function(){
+			var idIndex = this.getAttribute("id").substr(7);
+			$("#cancelButton" + idIndex).slideDown(transition);
+			$("#descriptForm" + idIndex).slideDown(transition);
+		});
+		
+		cancelButton.click(function(){
+			var idIndex = this.getAttribute("id").substr(12);
+			$("#cancelButton" + idIndex).slideUp(transition);
+			$("#descriptForm" + idIndex).slideUp(transition);
+			$("#textField" + idIndex).slideUp(transition);
+			$("#textInput" + idIndex).value("");
+		});
+		
+	}
 });
